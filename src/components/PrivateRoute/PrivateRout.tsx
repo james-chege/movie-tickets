1d6f67d95984097e13f2c43b1f9797019608e685
james-chege <jymeschege@gmail.com>
2020-09-04 06:37:49 +0300

    add private route wrapper

---
+++ src/components/PrivateRoute/PrivateRout.tsx
@@ -0,0 +1,23 @@
+import React, {FC} from "react";
+import { Route, Redirect } from "react-router-dom";
+import authUser from "../../utils/authUser.util";
+
+const PrivateRoute: FC<PrivateRouteProp> = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props => {
+           return authUser() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/"
+                    }}
+                />
+            )
+        }
+        }
+    />
+);
+
+export default PrivateRoute;
