deee5a3838faac32160576e17af4c4c8dd4ebdf4
James Chege <jymeschege@gmail.com>
2020-10-29 15:32:34 +0300

    remove junk

--- src/utils/api.util.ts
+++ src/utils/api.util.ts
@@ -7,7 +7,6 @@ const uri = process.env.REACT_APP_API_URL
 
 export const authUserHeader = () => {
     const token = authUser();
-    console.log({token})
     if (token) {
         return {
             Authorization: `Bearer ${token}`,
